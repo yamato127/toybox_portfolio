9911078aa498101403c94345540f9d983aecc195
yamato127 <kazu.92127@gmail.com>
2021-10-07 22:13:55 +0900

    #1

---
+++ www/html/order.php
@@ -0,0 +1,39 @@
+<?php
+
+// 設定ファイル読み込み
+require_once '../conf/const.php';
+// 関数ファイル読み込み
+require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'db.php';
+require_once MODEL_PATH . 'user.php';
+require_once MODEL_PATH . 'cart.php';
+require_once MODEL_PATH . 'order.php';
+
+// セッション開始
+session_start();
+
+// CSRFトークンの生成
+$csrf_token = get_csrf_token();
+
+// ログインしていなければ
+if(is_logined() === false){
+    // ログインページにリダイレクト
+    redirect_to(LOGIN_URL);
+}
+
+// DBに接続（PDOを取得）
+$db = get_db_connect();
+
+// ログインユーザーのデータを取得
+$login_user = get_login_user($db);
+
+// 注文履歴一覧を取得
+$orders = get_orders($db, $login_user['user_id'], is_admin($login_user));
+
+// カート内の全商品データを取得
+$carts = get_user_carts($db, $login_user['user_id']);
+// カート内商品の合計数量を取得
+$total_amount = get_total_cart_amount($carts);
+
+// ビューの読み込み
+include_once VIEW_PATH . 'order_view.php';
